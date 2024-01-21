c670c41ce2d071fd52e9ef57b60991373929ffb9
Younghun Ko <koyo@koyo.kr>
2024-01-22 01:16:04 +0900

    chore: Spring Boot 애플리케이션 생성

---
+++ backend/src/main/java/coffee/ssafy/ssafee/SsafeeApplication.java
@@ -0,0 +1,16 @@
+package coffee.ssafy.ssafee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication()
+@ConfigurationPropertiesScan
+public class SsafeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafeeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/coffee/ssafy/ssafee/SsafeeApplicationTests.java
@@ -0,0 +1,15 @@
+package coffee.ssafy.ssafee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ConfigurationPropertiesScan
+class SsafeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
