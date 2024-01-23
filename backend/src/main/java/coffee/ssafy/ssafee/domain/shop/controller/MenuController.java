1cc463358e6cc5846f73162ec0758d2710bdb9e5
Younghun Ko <koyo@koyo.kr>
2024-01-23 13:19:52 +0900

    feat: Menu API 구현

--- backend/src/main/java/coffee/ssafy/ssafee/domain/shop/controller/MenuController.java
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/shop/controller/MenuController.java
@@ -1,7 +1,10 @@
 package coffee.ssafy.ssafee.domain.shop.controller;
 
+import coffee.ssafy.ssafee.domain.shop.dto.MenuDetailDto;
 import coffee.ssafy.ssafee.domain.shop.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController {
 
     private final MenuService menuService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MenuDetailDto> getMenu(Long id) {
+        return ResponseEntity.ok().body(menuService.findMenuById(id));
+    }
 
 }
--- backend/src/main/java/coffee/ssafy/ssafee/domain/shop/service/MenuService.java
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/shop/service/MenuService.java
@@ -1,15 +1,12 @@
 package coffee.ssafy.ssafee.domain.shop.service;
 
 import coffee.ssafy.ssafee.domain.shop.dto.MenuDetailDto;
-import coffee.ssafy.ssafee.domain.shop.dto.MenuDto;
 import coffee.ssafy.ssafee.domain.shop.exception.ShopErrorCode;
 import coffee.ssafy.ssafee.domain.shop.exception.ShopException;
 import coffee.ssafy.ssafee.domain.shop.mapper.ShopMapper;
 import coffee.ssafy.ssafee.domain.shop.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +15,9 @@ public class MenuService {
     private final MenuRepository menuRepository;
     private final ShopMapper shopMapper;
 
-    public MenuDetailDto findShopById(Long id) {
+    public MenuDetailDto findMenuById(Long id) {
         return shopMapper.INSTANCE.toDetailDto(menuRepository.findById(id)
                 .orElseThrow(() -> new ShopException(ShopErrorCode.NOT_EXISTS_SHOP)));
-    }
-
-    public List<MenuDto> findAllShop() {
-        return menuRepository.findAll().stream()
-                .map(shopMapper.INSTANCE::toDto)
-                .toList();
     }
 
 }
