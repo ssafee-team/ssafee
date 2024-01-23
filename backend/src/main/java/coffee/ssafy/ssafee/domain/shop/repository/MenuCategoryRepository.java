b8c4a1c4cafdeacffd1c52a2d1bd08bd789e51fd
Younghun Ko <koyo@koyo.kr>
2024-01-23 15:17:14 +0900

    remove: 사용하지 않는 repository 삭제

--- backend/src/main/java/coffee/ssafy/ssafee/domain/shop/repository/MenuCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package coffee.ssafy.ssafee.domain.shop.repository;
-
-import coffee.ssafy.ssafee.domain.shop.entity.MenuCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuCategoryRepository extends JpaRepository<MenuCategory, Long> {
-}
--- backend/src/main/java/coffee/ssafy/ssafee/domain/shop/repository/OptionCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package coffee.ssafy.ssafee.domain.shop.repository;
-
-import coffee.ssafy.ssafee.domain.shop.entity.OptionCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionCategoryRepository extends JpaRepository<OptionCategory, Long> {
-}
