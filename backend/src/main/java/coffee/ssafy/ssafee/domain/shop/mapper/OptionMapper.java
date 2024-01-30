3522b27c3cebbf63117fb6c2eb52cab73cdad7e5
양희승 <heeseung0223@naver.com>
2024-01-31 00:52:03 +0900

    feat: 옵션 조회 기능구현

---
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/shop/mapper/OptionMapper.java
@@ -0,0 +1,12 @@
+package coffee.ssafy.ssafee.domain.shop.mapper;
+
+import coffee.ssafy.ssafee.domain.shop.dto.response.OptionResponse;
+import coffee.ssafy.ssafee.domain.shop.entity.Option;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface OptionMapper {
+
+    OptionResponse optionToOptionResponse (Option option);
+
+}
