d4fa13f7ee74ff0b18ce3d27da4837541383e092
phc <phc5403@gmail.com>
2024-01-31 17:59:51 +0900

    feat: ChatController basic setting

---
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/chat/controller/ChatController.java
@@ -0,0 +1,61 @@
+package coffee.ssafy.ssafee.domain.chat.controller;
+
+import io.openvidu.java.client.*;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/v1/parties/{access_code}/openvidu")
+@RequiredArgsConstructor
+public class ChatController {
+    @Value("${OPENVIDU_URL}")
+    private String OPENVIDU_URL;
+
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openvidu;
+//    private SessionProperties properties = new SessionProperties.Builder().build();
+//    private Session session = openvidu.createSession(properties);
+
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    /**
+     *
+     * @param params The Session properties
+     * @return The Session ID
+     */
+    @PostMapping("/api/sessions")
+    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/sessions/{sessionId}/connections")
+    public ResponseEntity<String> createConnectdion(@PathVariable("sessionId") String sessionId, @RequestBody(required = false) Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(sessionId);
+
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+
+
+    }
+
+
+}
