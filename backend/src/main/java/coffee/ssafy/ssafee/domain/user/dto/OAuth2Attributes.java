e5c5e1007ba2b02bf55a2cfa34d06799ea7cacbb
고영훈 <koyo@koyo.kr>
2024-02-02 14:37:00 +0900

    feat: OAuth2 + JWT 구현

---
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/user/dto/OAuth2Attributes.java
@@ -0,0 +1,12 @@
+package coffee.ssafy.ssafee.domain.user.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record OAuth2Attributes(
+        String email
+) {
+}
---
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/user/dto/TokensInfo.java
@@ -0,0 +1,13 @@
+package coffee.ssafy.ssafee.domain.user.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record TokensInfo(
+        String accessToken,
+        String refreshToken
+) {
+}
