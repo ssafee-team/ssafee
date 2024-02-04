f5e6f36c5c7f0e2611773aea921e0b986aaa1487
Younghun Ko <koyo@koyo.kr>
2024-02-04 18:19:35 +0900

    fix: order-menu 생성시 participant 중복체크를 하지 않는 문제 수정

--- backend/src/main/java/coffee/ssafy/ssafee/domain/party/repository/ParticipantRepository.java
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/party/repository/ParticipantRepository.java
@@ -3,5 +3,10 @@ package coffee.ssafy.ssafee.domain.party.repository;
 import coffee.ssafy.ssafee.domain.party.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+
+    Optional<Participant> findByPartyIdAndName(Long id, String s);
+
 }
