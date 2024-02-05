cbb83c2053da7d8c4b00c47204962fb437c0d722
Younghun Ko <koyo@koyo.kr>
2024-02-05 12:11:07 +0900

    chore: pricipal null 이슈 해결

--- backend/src/main/java/coffee/ssafy/ssafee/config/SecurityConfig.java
+++ backend/src/main/java/coffee/ssafy/ssafee/config/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/v1/test").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/shops/**", "/api/v1/parties").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/shops/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/shops/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         http
@@ -69,7 +72,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://ssafy.coffee"));
         configuration.setAllowedOriginPatterns(List.of("http://localhost:*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
