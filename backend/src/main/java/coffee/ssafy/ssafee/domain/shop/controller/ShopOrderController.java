10ead883ec925a67ec086ec6bec92d0ce01f741c
양희승 <heeseung0223@naver.com>
2024-02-06 11:59:38 +0900

    임시

--- backend/src/main/java/coffee/ssafy/ssafee/domain/shop/controller/ShopOrderController.java
+++ backend/src/main/java/coffee/ssafy/ssafee/domain/shop/controller/ShopOrderController.java
@@ -5,10 +5,7 @@ import coffee.ssafy.ssafee.domain.shop.service.ShopOrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,17 @@ public class ShopOrderController {
         return ResponseEntity.ok().body(shopOrderService.getShopHistories(shopId));
     }
 
+//    @GetMapping("/{order_id}/confirm")
+//    @Operation(summary = "매니저 : 주문기록상세조회")
+//    public ResponseEntity<List<>>
+
+//    @PostMapping("/{order_id}/confirm")
+//    @Operation(summary = "매니저 : 주문확인")
+//    public ResponseEntity<Void> orderConfirm(
+//            @PathVariable("shop_id") Long shopId,
+//            @PathVariable("order_id") Long orderId) {
+//        shopOrderService.orderConfirm(shopId, orderId);
+//        return ResponseEntity.noContent().build();
+//    }
 
 }
