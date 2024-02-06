b7334583caa3b7b584a443bf1112019e8979a2bd
Younghun Ko <koyo@koyo.kr>
2024-02-06 22:57:14 +0900

    chore: DB 가게 이미지 누락 수정

--- db/insert-shops.sql
+++ db/insert-shops.sql
@@ -4,7 +4,7 @@ VALUES (
 '컴포즈커피-수완장덕점',
 '광주광역시 광산구 장덕동 1043 수완지구 호반베르디움 2차아파트 213(상가)동 105호',
 '062-710-4090',
-'https://img.ssafy.coffee/shop/1/7defc41dd14e09ceb88193538bd56d1f.jpg',
+'https://img.ssafy.coffee/1/02c725dc7afdf889440688026b9f78b6253678ff41842dafe976c7beb37d0057.jpg',
 FALSE,
 12000,
 FALSE
