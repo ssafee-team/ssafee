1dc4657ad2c9ad55cebf43f19dc6ab0cdd7d9c9a
phc <phc5403@gmail.com>
2024-02-10 08:57:24 +0900

    feat: Additional features for chat

--- frontend/src/components/room/chat/ChatView.vue
+++ frontend/src/components/room/chat/ChatView.vue
@@ -4,39 +4,80 @@
             <div style="padding: 5px">
                 <div class="cur-date">{{ getToday() }}</div>
             </div>
-            <div class="chat-messages">
-                <div v-for="(message, index) in recvList" :key="index" class="message-box">
-                    <strong>{{ message.userName }}</strong>
-                    <div class="message">
+            <div class="chat-messages" ref="chatScroll">
+                <!-- <div v-for="(message, index) in recvList" :key="index" class="message-box"> -->
+                <div
+                    v-for="(message, index) in recvList"
+                    :key="index"
+                    class="message-box"
+                    :class="{
+                        'right-align': isCurrentUser(message.userName),
+                        'left-align': !isCurrentUser(message.userName),
+                    }"
+                >
+                    <div class="user-box">
+                        <strong>{{ message.userName }}</strong>
+                    </div>
+                    <div
+                        class="message"
+                        :class="{
+                            'right-align-message': isCurrentUser(message.userName),
+                            'left-align-message': !isCurrentUser(message.userName),
+                        }"
+                    >
                         {{ message.content }}
+                    </div>
+                    <div class="time-box">
+                        {{ message.contentTime }}
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <div>
-            유저 이름 :
-            <input v-model="userName" type="text" />
-        </div> -->
         <div class="chat-input">
-            <textarea v-model="message" @keyup="sendMessage" placeholder="메세지를 입력하세요."></textarea>
-            <!-- <input v-model="message" type="text" @keyup="sendMessage" placeholder="메세지를 입력하세요." /> -->
+            <textarea v-model="message" @keyup="sendMessage" placeholder="메시지를 입력하세요."></textarea>
+            <!-- <input v-model="message" type="text" @keyup="sendMessage" placeholder="메시지를 입력하세요." /> -->
             <button v-on:click="sendMessageClick">전송</button>
+            <div v-if="errMessage" class="error-message">{{ errMessage }}</div>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, nextTick } from "vue";
 // import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
 export default {
     name: "ChatView",
     setup() {
-        const userName = ref("유저");
         const message = ref("");
         const recvList = ref([]);
-        let stompClient = null;
+        const errMessage = ref(""); // 채팅 입력에 관한 에러 메시지
+        const chatScroll = ref(null);
+        let userNumbers = [];
+
+        // 채팅 유저 번호 할당
+        const getUserNumber = () => {
+            if (userNumbers.length >= 300) {
+                throw new Error("채팅방이 가득 찼습니다.");
+            }
+            let randomUserNumber;
+            do {
+                randomUserNumber = Math.floor(Math.random() * 300) + 1;
+            } while (userNumbers.includes(randomUserNumber));
+
+            userNumbers.push(randomUserNumber);
+            console.log("현재 유저 수" + userNumbers);
+            return randomUserNumber;
+        };
+
+        let stompClient = null; // 소켓 통신
+        const randomUserNumber = getUserNumber();
+        const userName = ref(`유저 ${randomUserNumber}`);
+
+        const isCurrentUser = (messageUsername) => {
+            return messageUsername === userName.value;
+        };
 
         const sendMessage = (e) => {
             if (e.keyCode === 13 && userName.value !== "" && message.value !== "") {
@@ -54,12 +95,24 @@ export default {
 
         const send = () => {
             // console.log("Send message:", message.value);
+            if (message.value.trim().length === 0) {
+                errMessage.value = "메시지를 입력하세요.";
+                setTimeout(() => {
+                    errMessage.value = "";
+                }, 1000);
+                message.value = "";
+                return;
+            }
+
             if (stompClient && stompClient.connected) {
                 const msg = {
                     userName: userName.value,
-                    // userName: Math.random(),
                     content: message.value,
+                    // inputTime: new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString(),
+                    // inputTime: new Date().toISOString(),
+                    contentTime: getTime(),
                 };
+                // console.log("시간은요?" + msg.inputTime); // 2024-02-09T21:33:47.954Z
                 stompClient.send("/receive", JSON.stringify(msg), {});
             }
         };
@@ -75,8 +128,12 @@ export default {
                 (frame) => {
                     // console.log("소켓 연결 성공", frame);
                     stompClient.subscribe("/send", (res) => {
-                        // console.log("구독으로 받은 메시지 입니다.", res.body);
+                        console.log("구독으로 받은 메시지 입니다.", res.body);
                         recvList.value.push(JSON.parse(res.body));
+
+                        nextTick(() => {
+                            chatScroll.value.scrollTop = chatScroll.value.scrollHeight;
+                        });
                     });
                 },
                 (error) => {
@@ -97,6 +154,15 @@ export default {
             return year + "년 " + month + "월 " + days + "일 (" + day + ")";
         };
 
+        const getTime = () => {
+            let today = new Date();
+            let hours = ("0" + today.getHours()).slice(-2);
+            let minutes = ("0" + today.getMinutes()).slice(-2);
+            let seconds = ("0" + today.getSeconds()).slice(-2);
+            let timeString = hours + ":" + minutes + ":" + seconds;
+            return timeString;
+        };
+
         onMounted(() => {
             connect();
         });
@@ -111,23 +177,30 @@ export default {
             userName,
             message,
             recvList,
+            errMessage,
+            randomUserNumber,
+            chatScroll,
+            getUserNumber,
+            isCurrentUser,
             sendMessage,
             getToday,
+            getTime,
             sendMessageClick,
-            // send 함수를 템플릿에서 사용하지 않으므로 반환하지 않아도 됩니다.
         };
     },
 };
 </script>
 
 <style>
+/* 전체 채팅 창 높이 조절해야 함. */
 .chat-window {
     /* height: 100%; */
+    margin-bottom: auto;
 }
 
 .chat-body {
-    height: 500px;
-    max-height: 500px;
+    height: 700px;
+    max-height: 700px;
     border-radius: 10px;
     background-color: #97afba;
     border: 3px solid #97afba;
@@ -162,31 +235,54 @@ export default {
 }
 
 .message-box {
-    margin-bottom: 30px;
+    /* text-align: right; */
+    margin-bottom: 20px;
 }
 
-/* .message-box::after {
-} */
-
-.message-box > strong {
-    color: #344a53;
+.user-box {
+    margin-bottom: 5px;
 }
 
-.message {
-    /* 색이 좀 칙칙폭폭함 */
-    /* background-color: #d9d9d9; */
-    background-color: white;
+.right-align {
+    text-align: right;
+}
+
+.left-align {
+    text-align: left;
+}
+
+.right-align-message {
+    margin-left: auto;
+    margin-right: 5px;
+    padding: 5px;
+    word-wrap: break-word;
+    max-width: 80%;
+    width: fit-content;
+    background-color: yellow;
     border-radius: 10px;
     font-weight: bold;
     font-size: 18px;
-    padding: 5px;
-    /* 메세지 길이에 맞게 width를 줘야 함 */
-    word-wrap: break-word; /* 긴 텍스트일 경우 자동으로 줄 바꿈되도록 설정합니다. */
-    max-width: 80%; /*메시지 상자의 최대 너비를 설정합니다.*/
-    margin-top: 5px;
+}
+
+.left-align-message {
+    margin-right: auto;
     margin-left: 5px;
-    width: fit-content; /* 메세지 길이에 맞게 너비 조정*/
-    /* position: relative; */
+    padding: 5px;
+    background-color: white;
+    word-wrap: break-word;
+    max-width: 80%;
+    width: fit-content;
+    border-radius: 10px;
+    font-weight: bold;
+    font-size: 18px;
+}
+
+.message-box > div > strong {
+    color: #344a53;
+}
+
+.time-box {
+    font-size: 12px;
 }
 
 .chat-input {
@@ -224,6 +320,11 @@ textarea::placeholder {
     font-weight: bold;
     border-radius: 10px;
     border: 1px solid white;
+}
+
+.error-message {
+    color: red;
+    font-weight: bold;
 }
 
 /* 화면 폭이 768px 미만일 때 */
